40c2d40e7d30f6aacfb8e46df96225b3f7ec2cc1
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-08 17:34:28 +0530

    JS

---
+++ script.js
@@ -0,0 +1,29 @@
+// Filter JS
+
+$(document).ready(function(){
+    $(".filter-item").click(function(){
+        const value= $(this).attr("data-filter");
+        if(value == "all") {
+            $(".post-box").show("1000");
+        }
+        else{
+            $(".post-box")
+            .not("."+value)
+            .hide("1000");
+            $(".post-box")
+            .filter("."+value)
+            .show("1000");
+        }
+    });
+    // Add active button
+    $(".filter-item").click(function(){
+        $(this).addClass("active-filter").siblings().removeClass("active-filter");
+    });
+});
+
+// Header background changes while scrolling down
+let header = document.querySelector("header");
+
+window.addEventListener("scroll",() => {
+    header.classList.toggle("shadow",window.scrollY > 0);
+});
